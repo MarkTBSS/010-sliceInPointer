92cb4f89c6e6a7956960840e8fcc6a3117c3d163
MarkTBSS <marktbss@gmail.com>
2024-01-30 11:11:42 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func sliceChange(a []int) {
+	a[0] = 0
+	a = append(a, 4) // nothing happens
+}
+
+func sliceChangeByPointer(a *[]int) {
+	*a = append(*a, 4)
+}
+
+func main() {
+	a := make([]int, 0)
+	a = append(a, 1, 2, 3)
+	fmt.Println(a) // 1, 2, 3
+	sliceChange(a)
+	fmt.Println(a) // 0, 2, 3
+	sliceChangeByPointer(&a)
+	fmt.Println(a) // 0, 2, 3, 4
+}
